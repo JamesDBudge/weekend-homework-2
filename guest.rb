49ed262ae055e5aa902259af95544854b19958d7
James Budge <thebudgie@gmail.com>
2019-04-12 14:11:20 +0100

    guest class initialized and tested

--- guest.rb
+++ guest.rb
@@ -1,6 +1,13 @@
 class Guest
 
+  attr_reader :name, :favourite_song
+  attr_accessor :cash
 
+def initialize(name, favourite_song, cash)
+  @name = name
+  @cash = cash
+  @favourite_song = favourite_song
+end
 
 
 
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -6,19 +6,20 @@ require_relative('../guest.rb')
 class GuestTest < Minitest::Test
 
   def setup
-
-
-
-
-
-
-
-
+    @guest1 = Guest.new("James", "In the End" , 20.00)
   end
 
+  def test_guest_has_name
+    assert_equal("James", @guest1.name)
+  end
 
+  def test_guest_has_cash
+    assert_equal(20.00, @guest1.cash)
+  end
 
-
+  def test_guest_favourite_song
+    assert_equal("In the End", @guest1.favourite_song)
+  end
 
 
 
