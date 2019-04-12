63895275cd7f946486f430099f013dd3b8efbd8c
James Budge <thebudgie@gmail.com>
2019-04-12 13:49:07 +0100

    room initialised and tested

--- room.rb
+++ room.rb
@@ -1,5 +1,13 @@
 class Room
 
+  attr_reader :capacity, :room_fee
+  attr_accessor :playlist
+
+  def initialize(capacity, playlist, room_fee)
+    @capacity = capacity
+    @playlist = playlist
+    @room_fee = room_fee
+  end
 
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -2,23 +2,31 @@ require('minitest/autorun')
 require('minitest/rg')
 
 require_relative('../room.rb')
+require_relative('../song.rb')
 
 class RoomTest < Minitest::Test
 
   def setup
+    @song1 = Song.new("Flower of Scotland", 1.00)
+    @song2 = Song.new("Tragedy", 2.00)
+    @song3 = Song.new("Power of Love", 5.00)
 
+    @playlist1 = [@song1, @song2, @song3]
 
-
-
-
-
-
-
+    @room1 = Room.new(5, @playlist1, 10.00)
   end
 
+  def test_room_capacity
+    assert_equal(5, @room1.capacity)
+  end
 
+  def test_playlist_exists
+    assert_equal(@playlist1, @room1.playlist)
+  end
 
-
+  def test_room_fee_set
+    assert_equal(10.00, @room1.room_fee)
+  end
 
 
 
